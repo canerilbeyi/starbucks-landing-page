cd74e24970e3c79705b3a0356df8dd65b88d8e09
canerilbeyi <73815030+canerilbeyi@users.noreply.github.com>
2021-05-05 22:10:26 +0300

    Add files via upload

---
+++ star.js
@@ -0,0 +1,15 @@
+$('.yesil').click(function(){
+        $('.starbucks').attr('src','images/img1.png');
+        $(".circle").css("background","#017143")
+
+});
+$('.pembe').click(function(){
+        $('.starbucks').attr('src','images/img2.png');
+        $(".circle").css("background","#eb7495")
+
+});
+$('.kırmızı').click(function(){
+        $('.starbucks').attr('src','images/img3.png');
+        $(".circle").css("background","#d752b1")
+
+});
